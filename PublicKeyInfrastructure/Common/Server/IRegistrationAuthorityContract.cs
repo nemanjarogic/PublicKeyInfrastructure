b749d6a4f28311c6603af73b68d97eba0fc64689
nemanjarogic <nemanjarogic93@gmail.com>
2016-12-06 18:44:21 +0100

    PKI infrastruktura

--- PublicKeyInfrastructure/Common/Server/IRegistrationAuthorityContract.cs
+++ PublicKeyInfrastructure/Common/Server/IRegistrationAuthorityContract.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common.Server
 {
-    interface IRegistrationAuthorityContract
+    [ServiceContract]
+    public interface IRegistrationAuthorityContract
     {
     }
 }
---
+++ PublicKeyInfrastructure/RegistrationAuthority/RegistrationAuthorityService.cs
@@ -0,0 +1,21 @@
+﻿using Common.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationAuthority
+{
+    public class RegistrationAuthorityService : ChannelFactory<IRegistrationAuthorityContract>, IRegistrationAuthorityContract, IDisposable
+    {
+        private IRegistrationAuthorityContract factory;
+
+        public RegistrationAuthorityService(NetTcpBinding binding, EndpointAddress address)
+            : base(binding, address)
+        {
+
+        }
+    }
+}
