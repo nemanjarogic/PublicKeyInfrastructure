8e4ac3e121b645b6b9fd882fde5cb7ce954c269e
nemanjarogic <nemanjarogic93@gmail.com>
2016-12-06 18:21:06 +0100

    ClientService izmenjen.

---
+++ PublicKeyInfrastructure/Client/ClientService.cs
@@ -0,0 +1,21 @@
+﻿using Common.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientService : ChannelFactory<IClientContract>, IClientContract, IDisposable
+    {
+        private IClientContract factory;
+
+        public ClientService(NetTcpBinding binding, EndpointAddress address)
+			: base(binding, address)
+		{
+            
+        }
+    }
+}
